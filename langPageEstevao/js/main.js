db549238677a12c6ce59c200a4c30cc89ffc5d83
Estevao <estevaorssouza@hotmail.com>
2022-07-07 01:01:09 -0300

    first commit

---
+++ langPageEstevao/js/main.js
@@ -0,0 +1,13 @@
+
+
+document.getElementById('gitHub').addEventListener('click', () => {
+
+    window.open('https://github.com/EstevaoSilvaSouza', "_newtab")
+    console.log('clicado')
+})
+
+document.getElementById('linke').addEventListener('click', () => {
+
+    window.open('https://linkedin.com/EstevaoSilvaSouza', "_newtab")
+    console.log('clicado')
+})
